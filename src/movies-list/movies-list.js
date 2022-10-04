459fdb130b7730e1c66a031f8fc17e2ea83896e2
Anna Zubkova <annazubkowa12@gmail.com>
2022-10-04 16:50:24 +0400

    rating and genres

--- src/movies-list/movies-list.js
+++ src/movies-list/movies-list.js
@@ -77,7 +77,8 @@ function MoviesList({ moviesData, loading, error, network, sessionID }) {
   const errorMessage = error ? errorElement : null;
   const spinner = loading && !error ? loadIcon : null;
   const content = !loading ? elements : null;
-  const networkMessage = !network && !loading && moviesData.length !== 0 ? networkElement : null;
+  const networkMessage =
+    (!network && !loading) || (!network && !loading && moviesData.length !== 0) ? networkElement : null;
 
   return (
     <ul className="movies-list">
